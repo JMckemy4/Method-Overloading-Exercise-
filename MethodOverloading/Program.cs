93621b45df8949c7104bbad2274dfa9ef1fe9d5c
jared mckemy <jaredmkcemy4@gmail.com>
2023-08-31 12:04:30 -0700

    method overload

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel.Design;
+
+namespace MethodOverloading
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static int Add(int num1, int num2)
         {
-            Console.WriteLine("Hello, World!");
+            return num1 + num2;
         }
-    }
-}
+
+        public static double Add(double dbl1, double dbl2)
+        {
+            Console.WriteLine($"{dbl1 + dbl2.ToString()} +dollar");
+            return dbl1 + dbl2;
+        }
+
+        public static string Add(int num1, int num2, bool stayHard)
+        {
+            if (stayHard)
+            {
+                if (num1 + num2 > 1)
+                {
+                    Console.WriteLine($"{num1 + num2} dollars");
+                    return (num1 + num2).ToString() + "dollars";
+
+                }//insideif
+                else 
+                {
+                    Console.WriteLine($"{num1 + num2} dollar");
+                    return (num1 + num2).ToString() + "dollar";
+                }
+            }//outsideif
+
+            else
+            {
+                Console.WriteLine($"{num1 + num2}");
+                return (num1 + num2).ToString();
+
+            }//else
+
+        }//add3
+
+
+
+                    static void Main(string[] args)
+                    {
+                        Add(1, 2);
+                        Add(3.2, 2.999);
+                        Add(-1, 0, true);
+                    }//main
+                }//class
+            } //namespace
+        
+    
+        
+    
+
+
+    
+    
+
+
+        
